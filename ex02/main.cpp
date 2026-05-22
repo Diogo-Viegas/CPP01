fc852e78e0a54eb74df3841eaf4beb25ed82f0b0
Diogo Viegas <diogo.dc.viegas@gmail.com>
2026-05-22 20:00:51 +0100

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPRT = &str;
+    std::string& stringREF = str;
+
+    std::cout << "The memory address of str: " << &str << std::endl;
+    std::cout << "The memory address of stringPRT: " << stringPRT << std::endl;
+    std::cout << "The memory address of stringRef: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPRT: "<< *stringPRT << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
+    return (0);
+}
